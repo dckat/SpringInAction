852cfbcfb50ed8be24c2b4582ae99ac046f83366
dckat <kdc9619@naver.com>
2022-09-01 21:02:32 +0900

    feat: 특정 deliveryzip과 일치하는 주문 검색 메소드 커스터마이징

--- src/main/java/tacos/repository/OrderRepository.java
+++ src/main/java/tacos/repository/OrderRepository.java
@@ -3,5 +3,8 @@ package tacos.repository;
 import org.springframework.data.repository.CrudRepository;
 import tacos.domain.Order;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<Order, Long> {
+    List<Order> findByDeliveryZip(String deliveryZip);
 }
