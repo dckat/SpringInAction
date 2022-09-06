e2de1f3ce6a371b9d6505b57af8be7e2710b9da7
dckat <kdc9619@naver.com>
2022-09-06 21:52:47 +0900

    feat: 스프링 시큐리티 기본 클래스 구성하기

---
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package tacos.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .access("hasRole('ROLE_USER')")
+                .antMatchers("/", "/**").access("permitAll")
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
