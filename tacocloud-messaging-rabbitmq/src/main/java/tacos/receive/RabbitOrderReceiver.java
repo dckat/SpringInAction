8ac27c58b9e19782e6f6a07ca85dbb464d3fdbd4
dckat <kdc9619@naver.com>
2023-03-16 17:41:17 +0900

    feat: RabbitMQ 메시지 수신 구현

---
+++ tacocloud-messaging-rabbitmq/src/main/java/tacos/receive/RabbitOrderReceiver.java
@@ -0,0 +1,21 @@
+package tacos.receive;
+
+import ch.qos.logback.classic.pattern.MessageConverter;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import tacos.Order;
+
+@Component
+public class RabbitOrderReceiver {
+    private RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public RabbitOrderReceiver(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public Order receiveOrder() {
+        return (Order) rabbitTemplate.receiveAndConvert("tacocloud.order.queue");
+    }
+}
