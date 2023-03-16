79be2398a2cd75eccb359cf19144d072dd5b9b7e
dckat <kdc9619@naver.com>
2023-03-16 17:38:33 +0900

    feat: Service 어노테이션 추가

--- tacocloud-messaging-rabbitmq/src/main/java/tacos/send/RabbitOrderMessagingService.java
+++ tacocloud-messaging-rabbitmq/src/main/java/tacos/send/RabbitOrderMessagingService.java
@@ -7,8 +7,10 @@ import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import tacos.Order;
 
+@Service
 public class RabbitOrderMessagingService implements OrderMessagingService {
     private RabbitTemplate rabbitTemplate;
 
