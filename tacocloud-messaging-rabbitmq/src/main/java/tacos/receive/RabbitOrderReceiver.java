9dec9a289fa7044bdc40e0c1a6552530e7393f42
dckat <kdc9619@naver.com>
2023-03-16 17:42:43 +0900

    chore: 불필요한 import 제거

--- tacocloud-messaging-rabbitmq/src/main/java/tacos/receive/RabbitOrderReceiver.java
+++ tacocloud-messaging-rabbitmq/src/main/java/tacos/receive/RabbitOrderReceiver.java
@@ -1,6 +1,5 @@
 package tacos.receive;
 
-import ch.qos.logback.classic.pattern.MessageConverter;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
